a4ced388cae046c25a52987a54d0d8fc3f0e6b41
Ahmed Mugahed <ai4241612@deltauniv.edu.eg>
2026-09-13 01:03:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-def find_largest(numbers):
-    # TODO: implement
-    pass
+def find_largest(numbers)->list:
+    largest=numbers[0]
+    for i in numbers:
+       if i > largest:
+           largest=i
+    return (largest,":is a largest number")
 
-
-numbers = [4, 12, 7, 19, 3]
-
+numbers=[4,12,7,19,3]
+nega_numbers=[-1,-2,-3,-4]
 print(find_largest(numbers))
+            
